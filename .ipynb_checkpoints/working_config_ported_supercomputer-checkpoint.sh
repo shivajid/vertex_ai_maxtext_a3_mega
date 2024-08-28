3107cf69f8648516db4ef3d94e10b3e57f42da95
Shivaji Dutta <shivaji@aurius.io>
2024-08-28 17:27:33 +0000

    Adding new Cluster Test

--- .ipynb_checkpoints/working_config_ported_supercomputer-checkpoint.sh
+++
@@ -1,5 +0,0 @@
-curl -X POST \
-     -H "Authorization: Bearer $(gcloud auth print-access-token)" \
-     -H "Content-Type: application/json; charset=utf-8" \
-     -d @working_config_ported_supercomputer.json \
-     "https://us-central1-aiplatform.googleapis.com/v1/projects/google.com:vertex-training-dlexamples/locations/us-central1/customJobs"
